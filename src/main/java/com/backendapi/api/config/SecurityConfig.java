b8795835eef293e6d08f14632116e4cdac5a514e
Rahamt907 <rahmatforpersonal7@gmail.com>
2026-09-02 18:45:45 +0530

    Auth test done

--- src/main/java/com/backendapi/api/config/SecurityConfig.java
+++ src/main/java/com/backendapi/api/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -16,7 +17,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity , JwtAuthFilter jwtAuthFilter){
         httpSecurity
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth ->auth
@@ -24,7 +25,8 @@ public class SecurityConfig {
             .permitAll()
             .anyRequest()
             .authenticated()
-        );
+        )
+        .addFilterBefore(jwtAuthFilter,UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 }
--- src/main/java/com/backendapi/api/controller/RegisterController.java
+++ src/main/java/com/backendapi/api/controller/RegisterController.java
@@ -4,6 +4,7 @@ package com.backendapi.api.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -69,5 +70,9 @@ public class RegisterController {
         return new ResponseEntity<>(lr.getLoginResponseDto(), HttpStatus.ACCEPTED);
     }
 
-    
+    @GetMapping("/p")
+    String test(){
+        return "Hello Auth Bhai";
+    }
+
 }
